e113745b90c1118e9b0dfe221df8db8f5749282e
Olivier Rousseau-Guyot <zeachco@gmail.com>
2022-04-13 17:26:20 -0400

    adds map system

--- src/games/tower-def/index.ts
+++ src/games/tower-def/index.ts
@@ -2,19 +2,18 @@ import { THREE } from "../../utilities/three"
 import { Mob } from "./classes/Mob"
 import { Path } from "./classes/Path"
 import { Segment } from "./classes/Segment"
+import map from "./maps/default"
 
 export default () => {
-  const path = new Path([
-    [new Segment(-4, 0)],
-    [new Segment(-3, -2), new Segment(-2, 2), new Segment(-1, -2)],
-    [new Segment(-1, -2)],
-    [new Segment(0, 1), new Segment(0, 0)],
-    [new Segment(3, 2)],
-    [new Segment(7, 3)],
-    [new Segment(3, 4)],
-    [new Segment(8, 5)],
-    [new Segment(3, 6)],
-  ])
+  const paths = map.paths.map(
+    (pathConfig) =>
+      new Path(
+        pathConfig.segments.map((waypoints) => {
+          console.log({ waypoints })
+          return waypoints.map(([x, y]) => new Segment(x, y))
+        })
+      )
+  )
 
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(
@@ -30,20 +29,22 @@ export default () => {
 
   const mobs: Mob[] = []
 
-  for (var i = 0; i < 40; i++) {
-    const mob = new Mob(path)
-    scene.add(mob.mesh)
-    mobs.push(mob)
-  }
+  paths.forEach((path) => {
+    for (var i = 0; i < 40; i++) {
+      const mob = new Mob(path)
+      scene.add(mob.mesh)
+      mobs.push(mob)
+    }
 
-  path.forEach((s) => scene.add(s.mesh))
+    path.forEach((s) => scene.add(s.mesh))
+  })
 
-  camera.position.z = 15
+  camera.position.z = 20
 
   function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    path.update()
+    paths.forEach((path) => path.update())
     mobs.forEach((mob) => mob.update())
   }
   animate()
