c20c2c30415c1ac25cfd2a457b9b96eeb21f156f
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2022-08-26 12:04:59 -0400

    changes colors and ball rules

--- src/games/ping-pong/Ball.ts
+++ src/games/ping-pong/Ball.ts
@@ -1,20 +1,25 @@
 export class Ball {
-  public vx: number
-  public vy: number
+  public vx: number = 0
+  public vy: number = 0
   public acc = 1.001
+  private color: string
 
   constructor(public x: number, public y: number, public size: number) {
     const angle = Math.random() * Math.PI * 2
     const speed = Math.random() * 3 + 2
-    this.vx = Math.cos(angle) * speed
-    this.vy = Math.sin(angle) * speed
+    setTimeout(() => {
+      this.vx = Math.cos(angle) * speed
+      this.vy = Math.sin(angle) * speed
+    }, Math.random() * 3000)
+    const colorAngle = Math.round(Math.random() * 360)
+    this.color = `hsl(${colorAngle}, 100%, 50%)`
   }
 
   render(ctx: CanvasRenderingContext2D) {
     this.x += this.vx
     this.y += this.vy
 
-    ctx.fillStyle = "#ff000088"
+    ctx.fillStyle = this.color
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
     ctx.fill()
--- src/games/ping-pong/index.ts
+++ src/games/ping-pong/index.ts
@@ -1,58 +1,77 @@
-import { Ball } from "./Ball"
-import { CollisionBox } from "./CollisionBox"
+import { Ball } from "./Ball";
+import { CollisionBox } from "./CollisionBox";
 
 export default () => {
-  var canvas = document.createElement("canvas")
-  document.body.appendChild(canvas)
-  var ctx = canvas.getContext("2d")
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+  const ctx = canvas.getContext("2d")!;
+  if (!ctx) throw new Error("no 2d context");
 
-  var GW = (canvas.width = 800)
-  var GH = (canvas.height = 600)
+  const GW = (canvas.width = 800);
+  const GH = (canvas.height = 600);
 
-  const leftPlayer = new CollisionBox(50, 150, 20, 140)
-  const rightPlayer = new CollisionBox(GW - 50, 150, 20, 140)
+  let leftV = 0;
+  let rightV = 0;
 
-  const allBoxes: CollisionBox[] = [leftPlayer, rightPlayer]
-  const allBalls: Ball[] = []
+  const leftPlayer = new CollisionBox(50, 150, 20, 140);
+  const rightPlayer = new CollisionBox(GW - 50, 150, 20, 140);
 
-  for (var i = 0; i < 5; i++) {
-    const x = (Math.random() * GW) / 2 + GW / 4
-    const y = Math.random() * GH
-    const area = Math.random() * 150
-    allBoxes.push(new CollisionBox(x, y, 5 + area, 5 + (150 - area)))
+  const allBoxes: CollisionBox[] = [leftPlayer, rightPlayer];
+  const allBalls: Ball[] = [];
+
+  for (var i = 0; i < 10; i++) {
+    const x = (Math.random() * GW) / 2 + GW / 4;
+    const y = Math.random() * GH;
+    const area = Math.random() * 150;
+    // allBoxes.push(new CollisionBox(x, y, 5 + area, 5 + (150 - area)));
+    allBalls.push(new Ball(GW / 2, GH / 2, 25));
   }
-  allBalls.push(new Ball(GW / 2, GH / 2, 25))
 
-  renderGame()
+  renderGame();
 
   function renderGame() {
-    requestAnimationFrame(renderGame)
-    ctx.clearRect(0, 0, GW, GH)
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
-    ctx.fillRect(0, 0, GW, GH)
+    requestAnimationFrame(renderGame);
+    ctx.clearRect(0, 0, GW, GH);
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fillRect(0, 0, GW, GH);
 
     // desiner les obstacles
     allBoxes.forEach((box) => {
-      box.render(ctx)
+      box.render(ctx);
       // box.getCollitionNormal(ball.x, ball.y, ball.size)
 
       // if (ballX < 0 + ballSize / 2) ballVX = Math.abs(ballVX * ballAcc) // left
       // if (ballY < 0 + ballSize / 2) ballVY = Math.abs(ballVY * ballAcc) // top
       // if (ballX > GW - ballSize / 2) ballVX = -Math.abs(ballVX * ballAcc) // right
       // if (ballY > GH - ballSize / 2) ballVY = -Math.abs(ballVY * ballAcc) // bottom
-    })
 
-    allBalls.forEach((ball) => {
-      leftPlayer.y = ball.y
-      rightPlayer.y = ball.y
+      void 2;
+    });
 
-      allBoxes.forEach((box) => box.getCollitionNormal(ball))
+    const sorted = allBalls.sort((a, b) => a.x - b.x);
+    const last = sorted.length - 1;
+    const speed = 1;
+    const pad = 25;
+    if (sorted[0].y > leftPlayer.y + pad) leftV += speed;
+    if (sorted[0].y < leftPlayer.y - pad) leftV -= speed;
+    if (sorted[last].y > rightPlayer.y + pad) rightV += speed;
+    if (sorted[last].y < rightPlayer.y - pad) rightV -= speed;
+    leftPlayer.y += leftV *= 0.9;
+    rightPlayer.y += rightV *= 0.9;
 
-      if (ball.x < 0 + ball.size / 2) ball.vx = Math.abs(ball.vx * ball.acc) // left: ;
-      if (ball.y < 0 + ball.size / 2) ball.vy = Math.abs(ball.vy * ball.acc) // top
-      if (ball.x > GW - ball.size / 2) ball.vx = -Math.abs(ball.vx * ball.acc) // right
-      if (ball.y > GH - ball.size / 2) ball.vy = -Math.abs(ball.vy * ball.acc) // bottom
-      ball.render(ctx)
-    })
+    allBalls.forEach((ball, index) => {
+      allBoxes.forEach((box) => box.getCollitionNormal(ball));
+
+      // if (ball.x < 0 + ball.size / 2) ball.vx = Math.abs(ball.vx * ball.acc); // left
+      // if (ball.x > GW - ball.size / 2) ball.vx = -Math.abs(ball.vx * ball.acc) // right
+      if (ball.y < 0 + ball.size / 2) ball.vy = Math.abs(ball.vy * ball.acc); // top
+      if (ball.y > GH - ball.size / 2) ball.vy = -Math.abs(ball.vy * ball.acc); // bottom
+      ball.render(ctx);
+      if (ball.x < 0 + ball.size / 2 || ball.x > GW - ball.size / 2) {
+        // left
+        allBalls.splice(index, 1);
+        allBalls.push(new Ball(GW / 2, GH / 2, 25));
+      }
+    });
   }
-}
+};
