63c03f4a5bd74a29da653a9dec8123d749238050
Olivier Rousseau-Guyot <zeachco@gmail.com>
2022-12-15 14:02:10 -0500

    switched to faction system

--- src/games/cells/classes/Cell.ts
+++ src/games/cells/classes/Cell.ts
@@ -1,202 +1,177 @@
-import { lerp, getAngle, angleOffset, vecLength } from "../../../utilities/math";
+import { lerp, getAngle, angleOffset, vecLength, randInt } from "../../../utilities/math";
+import { factionOffset, FACTIONS, MAX_FACTIONS } from "../factions";
 import { NeuralNetwork } from "../neural-network/NeuralNetwork";
 
-interface Enemy {
-    ref: Cell;
+interface Target {
+    x: number;
+    y: number;
     distance: number;
-    relativeAngle: number;
-    mouthDistance: number
+    opportunity: number
 }
 
 export class Cell {
-    public static TRACK_NB = 10
-    public static RADIUS = 15
-    public static MAX_NB = 10
+    public static TRACK_NB = 3
+    public static RADIUS = 10
+    public static MAX_NB = 20
 
-    public label = ''
+    public faction: number = randInt(0, MAX_FACTIONS - 1);
+
+    public label = this.faction + ''
     public x = 0;
     public y = 0;
+    public vx = 0;
+    public vy = 0;
     public a = 0;
     public va = 0;
     public s = 0;
     public focused = false
     public dead = false
-    public enemies: Enemy[] = []
+    public others: Target[] = []
     public energy = 100
     public atk = 0;
     public vatk = 0;
 
-    public brain = new NeuralNetwork(Cell.TRACK_NB * 3, 4)
+    public brain = new NeuralNetwork(Cell.MAX_NB * 2, 3, [Cell.MAX_NB, 5])
 
     constructor(instance: Partial<Cell> = {}) {
         Object.assign(this, instance);
     }
 
-    public attack() {
-        if (this.dead || this.atk > 1) return
-        this.energy -= 10
-        this.vatk = 20;
-        this.brain.score -= 1
-    }
-
     public forward() {
-        this.s += .03
+        this.vx += Math.cos(this.a) * .2
+        this.vy += Math.sin(this.a) * .2
         this.brain.score -= 0.01
     }
-
     public turnLeft() {
-        this.va -= Math.min(0.05, 1 / this.radius)
+        this.va -= 0.02
     }
     public turnRight() {
-        this.va += Math.min(0.05, 1 / this.radius)
+        this.va += 0.02
     }
-
     public get radius() {
-        return Cell.RADIUS;// + this.energy / 100
+        return Cell.RADIUS + this.energy / 20
     }
 
     public update() {
         this.s *= 0.99
-        this.va *= 0.7
+        this.va *= 0.8
+        this.vx *= 0.9
+        this.vy *= 0.9
         this.vatk *= 0.9
         this.atk = (this.atk + this.vatk) * .7
         if (this.dead) return
         this.energy -= this.s / 50
         this.energy -= this.va / 100
-        this.x += Math.cos(this.a) * this.s
-        this.y += Math.sin(this.a) * this.s
+        this.x += this.vx
+        this.y += this.vy
         this.a += this.va
         if (this.a > Math.PI) this.a -= Math.PI * 2
         if (this.a < -Math.PI) this.a += Math.PI * 2
+        if (this.brain.score < -25) this.dead = true
     }
 
-    public updateEnemies(cells: Cell[]) {
-        this.enemies = []
+    public updateTargets(others: Cell[], gw, gh) {
+        this.others = []
         if (this.dead) return;
 
-        for (let index = 0; index < cells.length; index++) {
-            const cell = cells[index];
-            if (cell === this || cell.dead) continue;
+        for (let index = 0; index < others.length; index++) {
+            const other = others[index];
+            if (other.dead || other.faction === this.faction) continue;
+            const distance = vecLength(this.x - other.x, this.y - other.y)
+            let opportunity = 0
+            const isPredator = factionOffset(this.faction, 1) === other.faction
+            const isPrey = factionOffset(this.faction, -1) === other.faction
+            if (isPredator) opportunity--
+            if (isPrey) opportunity++
+            if (distance < this.radius + other.radius) {
+                // Collide with predator
+                if (factionOffset(this.faction, 1) === other.faction) {
+                    this.dead = true
+                    this.brain.score -= 10
+                }
 
-            const [selfMouthX, selfMouthY] = this.projectedPosition();
-            const [foeMouthX, foeMouthY] = cell.projectedPosition();
-
-            const distance = vecLength(selfMouthX - cell.x, selfMouthY - cell.y)
-            const mouthDistance = vecLength(this.x - foeMouthX, this.y - foeMouthY)
-
-            if (distance < this.mouthSize + cell.radius) {
-                this.eat(cell)
+                // Collide with prey 
+                if (factionOffset(this.faction, -1) === other.faction) {
+                    other.dead = true
+                    this.brain.score += 10
+                }
             }
 
-            const [bx, by] = this.relativePosition(cell)
+            let x = other.x
+            let y = other.y
+            if (x - this.x > gw / 2) x -= gw
+            if (x - this.x < gw / -2) x += gw
+            if (this.y - y < gh / -2) y -= gh
+            if (this.y - y > gh / 2) y += gh
 
-            const globalAngle = getAngle(bx, by)
-            const relativeAngle = angleOffset(this.a, globalAngle)
-
-
-            this.enemies.push({
-                ref: cell,
+            this.others.push({
+                x,
+                y,
                 distance,
-                relativeAngle,
-                mouthDistance,
+                opportunity,
             })
         }
 
-        this.enemies.sort((a, b) => a.distance - b.distance)
+        this.others.sort((a, b) => a.distance - b.distance)
 
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
         if (this.focused) this.drawRelations(ctx)
-        const frontAngle = Math.PI * lerp(0.15, .4, this.atk / 50)
+        this.drawBody(ctx)
+        this.drawScore(ctx)
+    }
 
-        if (this.dead) {
-            ctx.fillStyle = 'rgba(255, 255, 255, .05)'
-            ctx.strokeStyle = 'rgba(255, 255, 255, .25)'
-        } else {
-            ctx.strokeStyle = 'rgba(255, 255, 255, .8)'
-            ctx.fillStyle = this.focused ? 'rgba(255, 128, 128, .8)' : 'rgba(0, 128, 128, .8)'
-        }
+    private drawBody(ctx: CanvasRenderingContext2D) {
+        const alpha = this.focused ? 1 : .4
+        const target = factionOffset(this.faction, -1)
+        const frontAngle = Math.PI * .1
+        ctx.lineWidth = 3
+        ctx.strokeStyle = `hsla(${FACTIONS[this.faction]}, 100%, 70%, ${alpha})`
+        ctx.fillStyle = `hsla(${FACTIONS[this.faction]}, 100%, 50%, ${alpha})`
 
         ctx.save()
         ctx.translate(this.x, this.y)
         ctx.rotate(this.a)
         ctx.beginPath()
         // body
-        ctx.moveTo(0, 0)
+        ctx.moveTo(this.radius * .5, 0)
         ctx.arc(0, 0, this.radius, frontAngle, Math.PI * 2 - frontAngle)
-        ctx.lineTo(0, 0)
-        // mouth
-        ctx.arc(this.attkRange, 0, this.mouthSize, frontAngle, Math.PI * 2 - frontAngle)
-        ctx.lineTo(0, 0)
+        ctx.lineTo(this.radius * .5, 0)
+
         ctx.stroke()
         ctx.fill()
+        ctx.beginPath()
+
+        ctx.strokeStyle = `hsla(${FACTIONS[target]}, 100%, 80%, ${alpha})`
+        ctx.fillStyle = `hsla(${FACTIONS[target]}, 100%, 20%, ${alpha})`
+        ctx.lineTo(0, 0)
+        ctx.lineTo(this.radius + 5, 0)
+        ctx.stroke()
+        ctx.fill()
+
         ctx.restore()
-
-        this.drawScore(ctx)
-    }
-
-    private get mouthSize() {
-        return this.attkRange / 4
-    }
-
-    private get attkRange() {
-        return this.radius + this.atk
     }
 
     private drawRelations(ctx: CanvasRenderingContext2D) {
-        ctx.setLineDash([7, 3]);
+        ctx.setLineDash([3, 3]);
         ctx.lineWidth = 2
-        this.enemies.slice(0, Cell.TRACK_NB).forEach((target, index) => {
-            const alpha = 1 - index / Cell.TRACK_NB
+        this.others.slice(0, Cell.TRACK_NB).forEach((target, index) => {
+            const alpha = 1 / (1 + index)
             ctx.beginPath()
-            const [px, py] = this.projectedPosition()
-
-            if (target.distance <= target.mouthDistance) {
-                ctx.strokeStyle = `rgba(0, 128, 0, ${alpha})`
-            } else {
-                ctx.strokeStyle = `rgba(255, 128, 0, ${alpha})`
-            }
-
-            ctx.moveTo(px, py)
-            ctx.strokeStyle = `rgba(0, 128, 0, ${alpha})`
-            ctx.lineTo(target.ref.x, target.ref.y)
+            ctx.strokeStyle = target.opportunity > 0 ? `rgba(0, 255, 0, ${alpha})` : `rgba(255, 128, 0, ${alpha})`
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(target.x, target.y)
             ctx.stroke()
-
         })
         ctx.setLineDash([]);
-    }
-
-    private projectedPosition(projection = this.attkRange) {
-        const px = this.x + Math.cos(this.a) * projection
-        const py = this.y + Math.sin(this.a) * projection
-        return [px, py]
-    }
-
-    private relativePosition(enemy: Cell): [number, number] {
-        const [px, py] = this.projectedPosition()
-        const dx = px - enemy.x
-        const dy = py - enemy.y
-        return [dx, dy]
-    }
-
-    private eat(cell: Cell) {
-        const chunk = Math.max(this.attkRange, this.atk)
-        cell.energy -= chunk
-        this.energy += chunk
-        const speedDiff = this.s - cell.s
-        this.brain.score += chunk * speedDiff
-        cell.brain.score -= chunk * speedDiff
-
-        if (cell.energy < 2) {
-            cell.dead = true
-        }
     }
 
     private drawScore(ctx: CanvasRenderingContext2D) {
         ctx.fillStyle = 'white'
         ctx.textAlign = "center"
-        const offsetY = this.y > Cell.RADIUS * 4 ? this.y - Cell.RADIUS * 2 : this.y + Cell.RADIUS * 2
-        ctx.fillText(`${this.label} ${this.brain.score.toFixed()}`, this.x, offsetY)
+        ctx.textBaseline = 'middle'
+        const offsetY = this.y > Cell.RADIUS * 5 ? this.y - Cell.RADIUS * 2 : this.y + Cell.RADIUS * 2
+        if (this.focused) ctx.fillText(`${this.brain.score.toFixed(0)}`, this.x, offsetY)
     }
 }
--- src/games/cells/main.ts
+++ src/games/cells/main.ts
@@ -1,10 +1,10 @@
 
 import { createCanvas } from '../../utilities/dom';
 import { GamePad } from '../../utilities/inputs/Gamepad';
-import { angleOffset, getAngle, lerp, rand, randInt } from '../../utilities/math';
+import { rand, randInt } from '../../utilities/math';
 import { GameLoop } from '../../utilities/three/GameLoop';
 import { Cell } from './classes/Cell';
-import { NeuralNetwork } from './neural-network/NeuralNetwork';
+import { MAX_FACTIONS } from './factions';
 
 export default async () => {
   const gamepad = new GamePad()
@@ -19,65 +19,74 @@ export default async () => {
   let cells: Cell[] = []
 
   let currentTop = cells[0]
-  let newSpawn = 0;
 
   const dummy = new Cell()
   dummy.brain.loadFromFile('top')
 
   let bestSavedScore = dummy.brain.score || 0
 
-  console.log({ bestSavedScore })
-
   loop.play((_es, _dt) => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const factionTotal = new Array(MAX_FACTIONS).fill(0)
     cells.forEach((cell, index) => {
-      // cell.focused = index === controllerIndex
-      if (cell.focused) humanControl(cell)
-      else AIControl(cell)
+      factionTotal[cell.faction]++
+      // if (cell.focused) humanControl(cell)
+      // else AIControl(cell)
+      AIControl(cell)
+
+      cell.update();
+
+      // wrap
       if (cell.x > GW) cell.x = 0
       if (cell.x < 0) cell.x = GW
       if (cell.y > GH) cell.y = 0
       if (cell.y < 0) cell.y = GH
-      cell.update();
+
+      // if (cell.x > GW) cell.x = GW
+      // if (cell.x < 0) cell.x = 0
+      // if (cell.y > GH) cell.y = GH
+      // if (cell.y < 0) cell.y = 0
     })
 
-    cells.forEach(cell => cell.updateEnemies(cells))
+    factionTotal.forEach((total, index) => {
+      if (total < Cell.MAX_NB) {
+        const orgCell = cells.find(c => c.faction === index)
+        const x = orgCell?.x || randInt(0, GW)
+        const y = orgCell?.y || randInt(0, GH)
+        const a = orgCell?.a || rand(-Math.PI, Math.PI)
 
-    if (cells.length < Cell.MAX_NB) {
-      for (let i = 0; i < 10; i++) {
-
-        const cell =
-          new Cell({
-            a: rand(-Math.PI, Math.PI),
-            x: randInt(0, GW),
-            y: randInt(0, GH),
-            energy: rand(100, 150),
-            label: `${newSpawn++}`
-          })
+        const cell = new Cell({
+          x,
+          y,
+          a,
+          energy: 100,
+          faction: index
+        })
 
         cell.brain.loadFromFile('top')
-        cell.brain.randomize(i / 10)
+        cell.brain.mutationAmount = total / Cell.MAX_NB / (1 + bestSavedScore) * 500
+        cell.brain.randomize()
         cell.brain.score = 0
         cells.push(cell)
-
       }
-    }
+    })
 
-
-    cells.forEach(cell => cell.draw(ctx))
-    cells = cells.filter(cell => !cell.dead)
-
+    cells.forEach(cell => cell.updateTargets(cells, GW, GH))
     const scores = cells.concat().sort((a, b) => b.brain.score - a.brain.score)
+    cells.forEach(cell => {
+      cell.focused = cell === scores[0]
+      cell.draw(ctx)
+
+    })
+    cells = cells.filter(cell => !cell.dead)
 
     currentTop = scores[0]
     if (currentTop && currentTop.brain.score > bestSavedScore) {
       const newScore = currentTop.brain.score;
-
       console.log(`New score: ${bestSavedScore} > ${newScore.toFixed()} (+${(newScore - bestSavedScore).toFixed(3)})`)
-
       bestSavedScore = newScore
-      console.log(`saving top ${currentTop.label}`)
       currentTop.brain.saveToFile('top')
     }
 
@@ -87,20 +96,20 @@ export default async () => {
     if (gamepad.get('ArrowUp')) cell.forward()
     if (gamepad.get('ArrowLeft')) cell.turnLeft()
     if (gamepad.get('ArrowRight')) cell.turnRight()
-    if (gamepad.get('Space')) cell.attack()
+    // if (gamepad.get('Space')) cell.attack()
   }
 
   function AIControl(cell: Cell) {
     const inputs: number[] = []
-    cell.enemies.forEach((enemy) => {
+    cell.others.forEach((enemy) => {
       inputs.push(enemy.distance)
-      inputs.push(enemy.mouthDistance)
-      inputs.push(enemy.relativeAngle)
+      inputs.push(enemy.opportunity)
+      // inputs.push(enemy.relativeAngle)
     })
-    const [up, left, right, space] = cell.brain.feedForward(inputs)
+    const [up, left, right] = cell.brain.feedForward(inputs)
     if (up) cell.forward()
     if (left) cell.turnLeft()
     if (right) cell.turnRight()
-    if (space) cell.attack()
+    // if (space) cell.attack()
   }
 };
