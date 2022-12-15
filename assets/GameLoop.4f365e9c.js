17794b814b0dba2162308c69d03ec48b1cb7deeb
zeachco <zeachco@users.noreply.github.com>
2022-12-15 19:02:42 +0000

    deploy: 9c3b225807f3dce39013f5a24a9ec496fcd3fe29

---
+++ assets/GameLoop.4f365e9c.js
@@ -0,0 +1,1 @@
+import{C as e}from"./three.module.f7a5d9ae.js";class n{constructor(){this.clock=new e}play(o){const t=()=>{requestAnimationFrame(t);const c=this.clock.getDelta(),s=this.clock.getElapsedTime();o(s,c)};t()}}export{n as GameLoop};
