65c5899aa2024380643bce97aa0bcae48c4f5cd7
zeachco <zeachco@users.noreply.github.com>
2023-01-13 19:35:11 +0000

    deploy: a13030a8edeb062b0c5cc3cbc8d4c6aa5ccb3f53

--- assets/Central.06af8128.js
+++
@@ -1,1 +0,0 @@
-import{b as a,c as e,a as s}from"./three.module.13bf8b03.js";const o=new a(1,8),r=new e({color:"rgba(255, 255, 255, .3)",opacity:.8});class i{constructor(){this.mesh=new s(o,r),this.mesh.rotation.set(Math.PI*1.5,Math.PI*0,Math.PI*0)}update(t){this.mesh.rotation.z+=Math.PI*.5*t}}export{i as Central};
