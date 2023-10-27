9c1409662c0b7c705a8d55baa1a74cac6c79a295
Olivier <zeachco@gmail.com>
2023-10-27 03:40:11 -0400

    refactored setup

--- src/games/highway/classes/Config.ts
+++ src/games/highway/classes/Config.ts
@@ -1,7 +1,7 @@
 import { ModelsByLayerCount } from '../../../ai/utils';
 
 class Config {
-  public CAR_NB = 25;
+  public CAR_NB = 10;
   public AUTO_DISTRIBUTE_LAYERS = false;
   public MAX_MUTATION_LVL = .7;
   public MIN_MUTATION_LVL = .01;
